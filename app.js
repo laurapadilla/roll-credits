b84474f3e816abe94b37abf1215826897f7cebb6
Laura Padilla <lpadilla@base2.io>
2022-09-29 10:31:01 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const divScroll = document.querySelector("div.scroll");
+
+const names = divScroll.querySelectorAll("div");
+
+names.forEach((name) => {
+  const namesHeight = name.clientHeight;
+  const namesTotal = 12;
+  divScroll.appendChild(name.cloneNode(true));
+
+  const movementTimeline = gsap.timeline({
+    repeat: -1
+  });
+
+  movementTimeline.set(divScroll, { y: 0 }).to(divScroll, {
+    y: namesHeight * namesTotal * -1,
+    duration: 45,
+    ease: "linear"
+  });
+});
